6ccc19e91014a0f26441eba6787ee35d181f1c81
B madhanmohan <bmadhanmohan123@gmail.com>
2023-11-28 09:23:37 +0000

    changes

--- df_network.py
+++ df_network.py
@@ -3,6 +3,14 @@ from pyspark.sql import *
 def create_spark_session():
     spark=SparkSession.builder.appName("df event").master('local[*]').getOrCreate()
     return spark
+def read_eventlog():
+    event_df=spark.read.format('csv')\
+        .load('hdfs://cdhserver:8020/user/labuser/madhan/networkevent_1.log')\
+        .toDF('EventLogs')
+    return event_df
 if __name__ == '__main__':
     spark=create_spark_session()
-    print(spark)
+    #print(read_eventlog().show(truncate=False))
+    even_data_log=read_eventlog()
+    ip_add=r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
+    res_df=evnt_data
