c9e0652974af02941de85f14ec9b554bb9d285f1
B madhanmohan <bmadhanmohan123@gmail.com>
2023-11-28 10:44:08 +0000

    changes files

--- df_network.py
+++ df_network.py
@@ -1,5 +1,8 @@
 from pyspark import *
 from pyspark.sql import *
+from pyspark.sql.functions import regexp_extract, substring, col, length
+
+
 def create_spark_session():
     spark=SparkSession.builder.appName("df event").master('local[*]').getOrCreate()
     return spark
@@ -12,5 +15,17 @@ if __name__ == '__main__':
     spark=create_spark_session()
     #print(read_eventlog().show(truncate=False))
     even_data_log=read_eventlog()
+    #64.241.88.10 ip pattern
     ip_add=r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}'
-    res_df=evnt_data
+    #[07 / Mar / 2004:16:05:49 - 0800]  datepattern
+    dt_ext=r'\[(\d{2}/\w{3}/\d{4}:\d{2}:\d{2}:\d{2} -\d{4})\]'
+    url_date=r'"([^"]*)"'
+    bytes_col=r'(\d{5})$'
+    res_df=even_data_log\
+        .withColumn('ip_address',regexp_extract('EventLogs', ip_add,idx=0)) \
+        .withColumn('ip_Date', regexp_extract('EventLogs', dt_ext, idx=1)) \
+        .withColumn('url_address',regexp_extract('EventLogs',url_date , idx=1)) \
+        .withColumn('Status_code', substring('EventLogs',length(even_data_log["EventLog"]))-9,3)\
+        .withColumn('storage', regexp_extract('EventLogs',bytes_col , idx=1)) \
+        .select('ip_address','ip_Date','url_address','Status_code','storage')
+    print(res_df.show(truncate=False))
