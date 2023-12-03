b921103c1ffcaa812642adb7369947d8269e1327
Admin <admin.push@hackerrank.workspace>
2023-12-03 09:22:41 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/job/pipeline.py
+++ src/main/job/pipeline.py
@@ -19,8 +19,7 @@ class PySparkJob(PySparkJobInterface):
         return self.spark
     def read_csv(self, input_path: str) -> DataFrame:
         # TODO: add your code here
-        self.sp= SparkSession.builder.getOrCreate().read.format("csv").option("header", "False").schema(vehicles).load(input_path).toDf()
-        print(self.sp)
+        self.sp= SparkSession.builder.getOrCreate().read.format("csv").option("header", "False").schema(vehicles).load(input_path)
         return self.sp
 
     def calc_average_efficiency(self, observed: DataFrame) -> DataFrame:
