6fe456bebeca26d4af0f9ed68bf1e4b1da43141f
Admin <admin.push@hackerrank.workspace>
2023-12-03 10:45:23 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/job/pipeline.py
+++ src/main/job/pipeline.py
@@ -27,8 +27,7 @@ class PySparkJob(PySparkJobInterface):
         # TODO: add your code here
         # data=observed.groupBy(observed.vehicleId).sum(observed.vehicleId)/observed['vehicleId'].count()
         # data=data.select('vehicleId','efficiency')
-        observed.createOrReplaceGlobalTempView('tab')
-        data =SparkSession.builder.getOrCreate().sql("select vehicleId,avg(fuelEfficiency) from tab group by vehicleId")
+        observed.createOrReplaceGlobalTempView('tab').sql("select vehicleId,avg(fuelEfficiency) from tab group by vehicleId")
         return data
 
     def find_faulty_vehicles(self, avg_observed: DataFrame, required: DataFrame) -> DataFrame:
