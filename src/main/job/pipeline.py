f4b0ccc79dbc8e69cc0ea7f3a07539b27d370581
Admin <admin.push@hackerrank.workspace>
2023-12-03 10:18:31 +0000

    Auto save changes before Run Tests
    Commit by HackerRank

--- src/main/job/pipeline.py
+++ src/main/job/pipeline.py
@@ -25,10 +25,10 @@ class PySparkJob(PySparkJobInterface):
 
     def calc_average_efficiency(self, observed: DataFrame) -> DataFrame:
         # TODO: add your code here
-        data=observed.groupBy(observed.vehicleId).sum(observed.vehicleId)/observed['vehicleId'].count()
-        data=data.select('vehicleId','efficiency')
-        #observed.createOrReplaceGlobalTempView('tab')
-        #data=SparkSession.builder.getOrCreate().sql("select vehicleId,sum(fuel_efficiency)/count(vehicleId) from tab group by vehicleId")
+        # data=observed.groupBy(observed.vehicleId).sum(observed.vehicleId)/observed['vehicleId'].count()
+        # data=data.select('vehicleId','efficiency')
+        observed.createOrReplaceGlobalTempView('tab')
+        data=SparkSession.builder.getOrCreate().sql("select vehicleId,avg(fuelEfficiency) from tab group by vehicleId")
         print(data.show())
 
         return 1
